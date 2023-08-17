7fc3bb44bd229349b2ec65f61f43bef65ad5925a
Shoeb Joarder <shoeb.joarder@uni-due.de>
2023-08-17 10:45:47 +0200

    Add scrape status to e3selector

--- backend/4-e3selector/app_e3selector/urls.py
+++ backend/4-e3selector/app_e3selector/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path("status/task/<str:task_id>/", check_task_status, name="check_task_status"),
     path("scrape/", scrape_e3, name="scrape_e3"),
     path(
         "courses-ratings/",  # replaced with "e3_courses_and_rating/" TODO: update frontend
--- backend/4-e3selector/app_e3selector/views/task_views.py
+++ backend/4-e3selector/app_e3selector/views/task_views.py
@@ -1,10 +1,27 @@
+from celery.result import AsyncResult
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
 import json
 
 from ..celery_tasks.tasks import scrape_e3_task
 
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def check_task_status(request, task_id):
+    task = AsyncResult(task_id)
+    if task.ready():
+        result = task.result
+        return JsonResponse(
+            {"status": "completed", "message": result}, status=status.HTTP_200_OK
+        )
+    else:
+        return JsonResponse({"status": "pending"}, status=status.HTTP_202_ACCEPTED)
 
+# TODO: Need to update to the django rest framework
 @csrf_exempt
 def scrape_e3(request):
     if request.method == "POST":
