3dcb2f5df240e822999975d9275a3e2db14bec32
Shoeb Joarder <shoeb.joarder@uni-due.de>
2023-08-21 16:40:05 +0200

    Add PostgreSQL settings to auth service

--- backend/3-auth/server/settings.py
+++ backend/3-auth/server/settings.py
@@ -20,6 +20,7 @@ import socket
 from py_eureka_client import eureka_client
 from django.core.management.commands.runserver import Command as runserver
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -125,12 +126,25 @@ WSGI_APPLICATION = "server.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": str(os.path.join(BASE_DIR, "db.sqlite3")),
+if bool(os.environ.get("POSTGRES_HOST", False)):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT", 5432),
+            "OPTIONS": json.loads(os.getenv("POSTGRES_OPTIONS", "{}")),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": str(os.path.join(BASE_DIR, "db.sqlite3")),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
