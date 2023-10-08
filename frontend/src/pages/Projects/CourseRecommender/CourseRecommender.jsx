301c0072fe7da4976f54629b93ed61d69405847f
Shoeb Joarder <shoeb.joarder@uni-due.de>
2023-10-08 11:20:58 +0200

    Update authentication of routes
    Renamed to CourseRecommender

--- frontend/src/pages/Projects/CourseRecommender/CourseRecommender.jsx
+++ frontend/src/pages/Projects/CourseRecommender/CourseRecommender.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Grid, Typography } from "@mui/material";
 
-import courseMatchLogo from "../../../assets/images/courseMatch-logo.png";
+import courseRecommenderLogo from "../../../assets/images/courseRecommender-logo.png";
 
-export default function CourseMatch() {
+export default function CourseRecommender() {
   return (
     <Grid container justifyContent="center" sx={{ py: 4, px: 2 }}>
       <Grid container sx={{ maxWidth: 1500, width: "100%" }} spacing={2}>
@@ -12,7 +12,7 @@ export default function CourseMatch() {
             <Grid
               item
               component="img"
-              src={courseMatchLogo}
+              src={courseRecommenderLogo}
               alt="Study Match Logo"
               xs={12}
               sm={7}
@@ -24,8 +24,8 @@ export default function CourseMatch() {
           <Grid container justifyContent="center" spacing={2}>
             <Grid item xs>
               <Typography variant="h5" align="center">
-                Course Match is a web application that allows students to find
-                courses and study programs based on their interests and
+                Course Recommender is a web application that allows students to
+                find courses and study programs based on their interests and
                 preferences.
               </Typography>
             </Grid>
